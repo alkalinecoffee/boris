3799d854efdec29fa1942a9a0864d99589b032af
alkalinecoffee <jwmartin83@gmail.com>
2013-01-24 18:03:19 -0500

    moved constants to separate file

--- lib/boris.rb
+++ lib/boris.rb
@@ -20,29 +20,11 @@ require 'boris/options'
 require 'boris/target'
 
 require 'boris/helpers/array'
+require 'boris/helpers/constants'
 require 'boris/helpers/hash'
 require 'boris/helpers/scrubber'
 require 'boris/helpers/string'
 
 module Boris
-
-  VENDOR_ADOBE      = 'Adobe Systems, Inc.'
-  VENDOR_AMD        = 'AMD, Inc.'
-  VENDOR_APC        = 'APC Corp.'
-  VENDOR_BROCADE    = 'Brocade Communications Corp.'
-  VENDOR_CISCO      = 'Cisco Systems, Inc.'
-  VENDOR_CITRIX     = 'Citrix Systems, Inc.'
-  VENDOR_DELL       = 'Dell Inc.'
-  VENDOR_EMULEX     = 'Emulex Corp.'
-  VENDOR_F5         = 'F5 Networks, Inc.'
-  VENDOR_HP         = 'Hewlett Packard, Inc.'
-  VENDOR_IBM        = 'IBM Corp.'
-  VENDOR_INTEL      = 'Intel Corp.'
-  VENDOR_MICROSOFT  = 'Microsoft Corp.'
-  VENDOR_ORACLE     = 'Oracle Corp.'
-  VENDOR_QLOGIC     = 'QLogic Corp.'
-  VENDOR_REDHAT     = 'Red Hat Inc.'
-  VENDOR_SUSE       = 'SUSE Linux GmbH'
-  VENDOR_VMWARE     = 'VMware, Inc.'
 
 end
