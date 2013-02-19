4af1e8d02bc166a1ecfc7fbc60b12be101e3a05d
Joe Martin <jwmartin83@gmail.com>
2013-02-19 15:57:24 -0500

    fix for devices asking for ssh password

--- lib/boris/connectors/ssh.rb
+++ lib/boris/connectors/ssh.rb
@@ -15,6 +15,11 @@ module Boris
       @ssh_options = options[:ssh_options]
       @ssh_options[:password] = @password if @password
 
+      @ssh_options[:auth_methods] = ['publickey']
+      if @password
+        @ssh_options[:auth_methods].concat(['keyboard-interactive', 'password'])
+      end
+
       if @ssh_options
         invalid_ssh_options = @ssh_options.keys - Net::SSH::VALID_OPTIONS
         raise ArgumentError, "invalid ssh option(s): #{invalid_ssh_options.join(', ')}" if invalid_ssh_options.any?
