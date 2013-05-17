a4896fc15e9e4c17b7f68cfd506a0fae90a10dbd
Joe Martin <jwmartin83@gmail.com>
2013-05-17 10:10:21 -0400

    add Profiler#available_profilers

--- lib/boris/profiler.rb
+++ lib/boris/profiler.rb
@@ -2,6 +2,16 @@ require 'boris/structure'
 require 'boris/helpers'
 
 module Boris; module Profilers
+
+  def self.available_profilers
+    ObjectSpace.each_object(Class).inject([]) do |result, item|
+      if item < Base && !(item.to_s =~ /windows/i && PLATFORM != :win32) && item.respond_to?(:matches_target?)
+        result << item
+      end
+      result
+    end
+  end
+
   class Base
     include Lumberjack
     include Structure
